e256c9f865f1fc551f497f107d171e540f0b1857
Fausto Fusse <faustofusse@gmail.com>
2019-08-11 23:33:08 -0300

    Funciona decente

--- prueba.py
+++ prueba.py
@@ -3,9 +3,9 @@ import pickle
 
 # print (configs.squad.squad)
 path = './DeepPavlov/deeppavlov/configs/squad/squad.json'
-path2 = './models/models/squad_model/model'
+pathMulti = './DeepPavlov/deeppavlov/configs/squad/squad_ru_bert_infer.json'
 
-model = build_model(path, download=True);
+model = build_model(pathMulti, download=True)
 model(['Hoss cocino una torta de manzana ayer a la noche.'], ['Que hizo Hoss?'])
-
+# https://github.com/deepmipt/DeepPavlov
 # python -m deeppavlov interact DeepPavlov/deeppavlov/configs/squad/squad_bert.json
