d712259675486863a8748b53785bb5b4a660229d
Fausto Fusse <faustofusse@gmail.com>
2019-09-26 23:23:46 -0300

    Empiezo el webserver

--- FaceTracking/camera2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-import imutils
-from imutils.object_detection import non_max_suppression
-
-from pyimagesearch.centroidtracker import CentroidTracker
-from imutils.video import VideoStream
-import argparse
-import time
