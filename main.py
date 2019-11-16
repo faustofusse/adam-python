925b2b32a7d69bec9a498ef87dbc85f9198e2cb0
Fausto Fusse <faustofusse@Faustos-MacBook-Pro.local>
2019-11-16 01:08:03 -0300

    Aplico sockets al stream de OpenCV

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, send, emit
+from camera import FaceDetectionCamera
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/holis', methods=['POST'])
+def post():
+    socketio.emit('values', {'hola': 3232})
+    return 'Holaaaaa'
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    # socketio.emit('values', {'hola': 3232})
+    return Response(gen(FaceDetectionCamera(socketio)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@socketio.on('holaa')
+def handle_message(message):
+    print('received message (hola): ' + str(message))
+
+
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
